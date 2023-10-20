317daf0b0f1655bb6dd03b7b0d13db16b0ce711d
Aditya <aditya.sasmal.dev@gmail.com>
2023-10-20 15:41:44 +0530

    Adds Player to screen with basic physics

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import sys
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
 class game:
     def __init__(self):
         pygame.init()
@@ -8,20 +11,21 @@ class game:
         pygame.display.set_caption("Shinobi Sprint")
         # Creates a window
         self.screen = pygame.display.set_mode((640,480))
+        # Loads empty surface and then we upscale it for pixel art
+        self.display = pygame.Surface((320,240))
 
-        # clock object will be usefull to limit fps to 60 
         self.clock = pygame.time.Clock()
+        
 
-        # Loads image
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        # Makes the background of the image transparent
-        self.img.set_colorkey((0,0,0))
-        self.img_pos = [160,260]
         self.movement = [False,False]
 
-        # Creates a Rectangle on screen 
-        self.collision_area = pygame.Rect(50,50,300,50)
 
+        self.assets = {
+            'player' : load_image('entities/player.png')
+        }
+
+        # Create player
+        self.player = PhysicsEntity(self,'player',(50,50),(8,15))
 
     #  we are running an infinite loop which will change the
     #  content onscreen based on inputs by user.
@@ -29,21 +33,10 @@ class game:
     def run(self):
         while True:
             # Clear the screen by filing with a colour
-            self.screen.fill((14,219,248))
+            self.display.fill((14,219,248))
 
-            # Create a rectangle around image in current frame
-            img_r = pygame.Rect(self.img_pos[0],self.img_pos[1],self.img.get_width(),self.img.get_height())
-
-            # Check if both collide and change colours
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen,(0,100,255),self.collision_area)
-            else:
-                pygame.draw.rect(self.screen,(0,50,155),self.collision_area)
-
-            # Trick to  change position of image on input
-            self.img_pos[1] += (self.movement[1] - self.movement [0])*5
-            # Display image on screen(blit)
-            self.screen.blit(self.img,self.img_pos)
+            self.player.update((self.movement[1] - self.movement[0],0))
+            self.player.render(self.display)
 
             # If the user presses the close button
             for event in pygame.event.get():
@@ -52,17 +45,18 @@ class game:
                     sys.exit()
                 # Move Stuff around
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
             # Updates the screen and limits the fps
+            self.screen.blit(pygame.transform.scale(self.display,self.screen.get_size()),(0,0))
             pygame.display.update()
             self.clock.tick(60)
 
